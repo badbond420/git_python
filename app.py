0b1000677da191c7c2d80b7c4b86e5710f55b388
bibek <badbond420@gmail.com>
2018-06-15 11:31:55 +0530

    new mul_num function

--- app.py
+++ app.py
@@ -7,5 +7,14 @@ def add_nums(input_list):
 
     return result
 
-arr = [9,7,8,9]
-print(add_nums(arr))
+
+def mul_nums(input_list):
+    result = 1
+    for num in input_list:
+        result *= num
+
+    return result
+
+if __name__ == "__main__":
+    arr = [9,7,8,9]
+    print(add_nums(arr))
